22103310e371ab57d5abe361d957141512a93632
akromjon <numonov.akromjon1998@gmail.com>
2024-02-06 15:51:04 +0500

    fix: removed routes in api

--- modules/Server/Routes/api.php
+++ modules/Server/Routes/api.php
@@ -8,9 +8,4 @@ Route::middleware(['token','version'])->prefix("/servers")->group(function () {
     Route::get("/", [ServerController::class, 'list']);
 
     Route::post("/{ip}/download", [ServerController::class, 'download']);
-
-    Route::get("/connected", [ServerController::class, 'connected']);
-
-    Route::get("/disconnected", [ServerController::class, 'disconnected']);
-
 });
